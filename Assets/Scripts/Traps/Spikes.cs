4c56078b6a69866b73ec65ed7f0ddee160e373f7
Klainis <136218846+Klainis@users.noreply.github.com>
2025-11-07 04:02:30 +0500

    Небольшое изменение логики получения урона врагам от шипов

--- Assets/Scripts/Traps/Spikes.cs
+++ Assets/Scripts/Traps/Spikes.cs
@@ -35,7 +35,7 @@ public class Spikes : MonoBehaviour
         else if (collider.gameObject.CompareTag("Enemy"))
         {
             enemy = collider.gameObject;
-            Destroy(enemy);
+            enemy.SendMessage("ApplyDamage", 9999);
         }
     }
 
