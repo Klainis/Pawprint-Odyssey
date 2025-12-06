e7709e3b5b6696e309b5841efecc0e771391247c
Klainis <136218846+Klainis@users.noreply.github.com>
2025-12-06 16:48:29 +0500

    Расставлены id кристаллов

--- Assets/Scripts/Core/EntryPoint.cs
+++ Assets/Scripts/Core/EntryPoint.cs
@@ -6,7 +6,7 @@ using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+//using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class EntryPoint : MonoBehaviour
 {
