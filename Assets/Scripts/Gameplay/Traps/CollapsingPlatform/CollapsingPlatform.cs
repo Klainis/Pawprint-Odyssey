739b19bc8ff2267444a6cb95dd5afd1e340b71af
Klainis <136218846+Klainis@users.noreply.github.com>
2025-12-16 02:24:07 +0500

    Телеграфы разрушающейся платформы

--- Assets/Scripts/Gameplay/Traps/CollapsingPlatform/CollapsingPlatform.cs
+++ Assets/Scripts/Gameplay/Traps/CollapsingPlatform/CollapsingPlatform.cs
@@ -4,14 +4,26 @@ using UnityEngine.U2D;
 
 public class CollapsingPlatform : MonoBehaviour
 {
-    [SerializeField] private float timeBeforeHide = 1.5f;
-    [SerializeField] private float timeToReveal = 1.5f;
+    [Header("Destroy parameters")]
+    [SerializeField] private float _timeBeforeHide = 1.5f;
+    [SerializeField] private float _timeToReveal = 1.5f;
+
+    [Header("Shake parameters")]
+    [SerializeField] private float _initialShakeDuration = 0.1f;
+    [SerializeField] private float _shakeMagnitude = 0.25f;
+
+    [Header("Particles")]
+    [SerializeField] private ParticleSystem _destroyParticale;
+
+    private ParticleSystem _destroyParticaleInstance;
 
     private SpriteShapeRenderer spriteRenderer;
     private Collider2D platformCollider;
     private Collider2D collapseTriggerCollider;
+    private Vector3 _initialPosition;
 
     private bool isHidden = false;
+    private float _shakeDuration;
 
     public bool IsHidden { get { return isHidden; } }
 
@@ -22,6 +34,12 @@ public class CollapsingPlatform : MonoBehaviour
         collapseTriggerCollider = GetComponentInChildren<CollapseTrigger>().GetComponent<Collider2D>();
     }
 
+    private void Start()
+    {
+        _initialPosition = transform.localPosition;
+        _shakeDuration = _timeBeforeHide;
+    }
+
     public void ChangePlatformState(bool toHidden)
     {
         isHidden = toHidden;
@@ -30,14 +48,37 @@ public class CollapsingPlatform : MonoBehaviour
         collapseTriggerCollider.enabled = !toHidden;
     }
 
+    public IEnumerator Shake()
+    {
+        while (_shakeDuration >= 0)
+        {
+            transform.localPosition = _initialPosition + Random.insideUnitSphere * _shakeMagnitude;
+            _shakeDuration -= Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localPosition = _initialPosition;
+    }
+
+    private void InstantiateParticles()
+    {
+        Quaternion particleRotation = new Quaternion(-90, 0, 0, 0);
+        _destroyParticaleInstance = Instantiate(_destroyParticale, transform.position, particleRotation);
+        Debug.Log(_destroyParticaleInstance.transform.rotation);
+    }
+
     public IEnumerator WaitAndChangeState()
     {
-        yield return new WaitForSeconds(timeBeforeHide);
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(Shake());
+        yield return new WaitForSeconds(_timeBeforeHide);
         ChangePlatformState(true);
+        InstantiateParticles();
 
-        yield return new WaitForSeconds(timeToReveal);
+        yield return new WaitForSeconds(_timeToReveal);
         while (Physics2D.OverlapBox(transform.position, platformCollider.bounds.size, 0, LayerMask.GetMask("Player")) != null)
             yield return null;
         ChangePlatformState(false);
+        _shakeDuration = _timeBeforeHide;
     }
 }
