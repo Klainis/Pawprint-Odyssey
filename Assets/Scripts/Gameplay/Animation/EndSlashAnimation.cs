6b866ea01b1ebeeebade56b9dc9b30acdcb516a5
Klainis <136218846+Klainis@users.noreply.github.com>
2026-01-07 21:36:22 +0500

    Закончена анимация ударов

--- Assets/Scripts/Gameplay/Animation/EndSlashAnimation.cs
+++ Assets/Scripts/Gameplay/Animation/EndSlashAnimation.cs
@@ -2,8 +2,18 @@ using UnityEngine;
 
 public class EndSlashAnimation : MonoBehaviour
 {
+    private Quaternion _slashRotationInitial;
+    private Vector3 _slashScaleInitial;
+
+    private void Awake()
+    {
+        _slashRotationInitial = GetComponent<Transform>().localRotation;
+        _slashScaleInitial = GetComponent<Transform>().localScale;
+    }
     public void OnSlashAnimationEnd()
     {
+        gameObject.transform.localRotation = _slashRotationInitial;
+        gameObject.transform.localScale = _slashScaleInitial;
         gameObject.SetActive(false);
     }
 
