1ba850526d69a20d0e2bd5285312febc476495c1
malinovbly <temanov0530@gmail.com>
2026-05-23 18:35:11 +0500

    _

--- Assets/Scripts/Gameplay/Story/EnablePimenDialogueInPoint.cs
+++ Assets/Scripts/Gameplay/Story/EnablePimenDialogueInPoint.cs
@@ -10,35 +10,43 @@ public class EnablePimenDialogueInPoint : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            PimenTalk wr = PimenView.Instance.gameObject.GetComponent<PimenTalk>();
+        if (!collision.gameObject.CompareTag("Player")) return;
 
-            if (_beforeFirstBoss)
-            {
-                wr.BeforeFirstBoss();
-                return;
-            }
-            if (_beforeTakeClaw)
-            {
-                wr.SawClaw();
-                return;
-            }
-            if (_beforeFirstFightRoom)
-            {
-                wr.BeforeFirstFightRoom();
-                return;
-            }
-            if (_afterFirstFightRoom)
-            {
-                wr.AfterFirstFightRoom();
-                return;
-            }
-            if (_beforeLastBoss)
-            {
-                wr.BeforeFinalBoss();
-                return;
-            }
+        var playerModel = PlayerView.Instance.PlayerModel;
+        PimenTalk wr = PimenView.Instance.gameObject.GetComponent<PimenTalk>();
+
+        if (_beforeFirstBoss && !playerModel.BeforeFirstBoss)
+        {
+            wr.BeforeFirstBoss();
+            playerModel.SetBeforeFirstBoss(true);
+            return;
+        }
+
+        if (_beforeTakeClaw && !playerModel.SawClaw)
+        {
+            wr.SawClaw();
+            playerModel.SetSawClaw(true);
+            return;
+        }
+
+        if (_beforeFirstFightRoom && !playerModel.BeforeFirstFightRoom)
+        {
+            wr.BeforeFirstFightRoom();
+            playerModel.SetBeforeFirstFightRoom(true);
+            return;
+        }
+
+        if (_afterFirstFightRoom)
+        {
+            wr.AfterFirstFightRoom();
+            return;
+        }
+
+        if (_beforeLastBoss && !playerModel.BeforeFinalBoss)
+        {
+            wr.BeforeFinalBoss();
+            playerModel.SetBeforeFinalBoss(true);
+            return;
         }
     }
 }
