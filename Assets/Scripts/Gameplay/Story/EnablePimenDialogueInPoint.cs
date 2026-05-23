0d137fa00580eef436886e2fddb58a0edbd3994a
Klainis <136218846+Klainis@users.noreply.github.com>
2026-05-23 18:21:52 +0500

    Разрешение конфликтов

--- Assets/Scripts/Gameplay/Story/EnablePimenDialogueInPoint.cs
+++ Assets/Scripts/Gameplay/Story/EnablePimenDialogueInPoint.cs
@@ -5,37 +5,38 @@ public class EnablePimenDialogueInPoint : MonoBehaviour
     [SerializeField] private bool _beforeFirstBoss = false;
     [SerializeField] private bool _beforeTakeClaw = false;
     [SerializeField] private bool _beforeFirstFightRoom = false;
+    [SerializeField] private bool _afterFirstFightRoom = false;
     [SerializeField] private bool _beforeLastBoss = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            var playerModel = PlayerView.Instance.PlayerModel;
             PimenTalk wr = PimenView.Instance.gameObject.GetComponent<PimenTalk>();
 
-            if (_beforeFirstBoss && !playerModel.BeforeFirstBoss)
+            if (_beforeFirstBoss)
             {
                 wr.BeforeFirstBoss();
-                playerModel.SetBeforeFirstBoss(true);
                 return;
             }
-            if (_beforeTakeClaw && !playerModel.SawClaw)
+            if (_beforeTakeClaw)
             {
                 wr.SawClaw();
-                playerModel.SetSawClaw(true);
                 return;
             }
-            if (_beforeFirstFightRoom && !playerModel.BeforeFirstFightRoom)
+            if (_beforeFirstFightRoom)
             {
                 wr.BeforeFirstFightRoom();
-                playerModel.SetBeforeFirstFightRoom(true);
                 return;
             }
-            if (_beforeLastBoss && !playerModel.BeforeFinalBoss)
+            if (_afterFirstFightRoom)
+            {
+                wr.AfterFirstFightRoom();
+                return;
+            }
+            if (_beforeLastBoss)
             {
                 wr.BeforeFinalBoss();
-                playerModel.SetBeforeFinalBoss(true);
                 return;
             }
         }
